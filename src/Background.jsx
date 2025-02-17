823bbe5aa7b02f6ebfc61841e2d455e6635761d2
ManishAmFour <ManishTewatia3020@gmail.com>
2025-02-17 19:16:42 +0530

    implemented the required features as it's suppose to be in the TODO List

---
+++ src/Background.jsx
@@ -0,0 +1,17 @@
+import "./Layout.css";
+
+function Background() {
+  return (
+    <div className="background">
+      <p className="title">To Do List</p>
+      <div className="project-sidebar">
+        <p className="project-title">Project Lists</p>
+      </div>
+      <div className="todo-list">
+        <p className="todo-title">Todo-Tasks</p>   
+      </div>
+    </div>
+  );
+}
+
+export default Background;
---
+++ src/Project.jsx
@@ -0,0 +1,101 @@
+import { useState, useRef } from "react";
+
+let ProjectName;
+let taskName;
+let TaskDescription;
+let DateValue;
+
+
+let ProjectCart = [];
+let ToDoCart = [];
+
+let date = new Date();
+let Year = date.getFullYear();
+let Month = String(date.getMonth() + 1).padStart(2, "0");
+let day = String(date.getDate()).padStart(2, "0");
+
+let fulldate = `${Year}-${Month}-${day}`;
+
+function TodoMaker() {
+  let [dateChange, setDateChange] = useState(fulldate);
+
+  taskName = useRef("task");
+  TaskDescription = useRef("empty");
+  DateValue = useRef('')
+
+  function DateChange(value) {
+    setDateChange(value);
+  }
+
+  return (
+    <div className="input-bar-div">
+      <input placeholder="name of the task"></input>
+      <textarea
+        ref={TaskDescription}
+        className="description-text"
+        placeholder="description"
+      />
+      <input
+        value={dateChange}
+        onChange={(e) => {
+          DateChange(e.target.value);
+        }}
+        ref={DateValue}
+        type="date"
+      />
+
+      <select>
+        <option>High priority</option>
+        <option>Medium priority</option>
+        <option>Low priority</option>
+      </select>
+    </div>
+  );
+}
+
+function ProjectDisplay() {
+  ProjectName = useRef("initial");
+  console.log(DateValue)
+  return (
+    <p className="project-input">
+      Project Name:- <input ref={ProjectName} />
+    </p>
+  );
+}
+
+function ProjectMaker() {
+  const [Switch, useSwitch] = useState("project");
+
+  function changeState() {
+    if (Switch === "project") {
+      if (ProjectName.current.value !== "") {
+        useSwitch("todo");
+        ProjectCart.push(ProjectName.current.value);
+        localStorage.setItem("project-list", JSON.stringify(ProjectCart));
+      }
+    } else {
+      if (
+        taskName.current.value !== "" &&
+        TaskDescription.current.value !== ""
+      ) {
+        useSwitch("project");
+        let NewTask = {
+          task: taskName.current.value,
+          description: TaskDescription.current.value,
+        };
+        ToDoCart.push(NewTask);
+        localStorage.setItem("todo-list", JSON.stringify(ToDoCart));
+      }
+    }
+  }
+
+  return (
+    <div className="input-query-bar">
+      {Switch === "project" ? <ProjectDisplay /> : <TodoMaker />}
+
+      <button onClick={changeState}>Enter</button>
+    </div>
+  );
+}
+
+export default ProjectMaker;
